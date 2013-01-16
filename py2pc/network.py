398e828b88e6676d70afcc6d4d5ab72a9d38c152
Joe Cruz <joe.cruz@rackspace.com>
2013-01-15 21:37:26 -0600

    Add network skeleton.

---
+++ py2pc/network.py
@@ -0,0 +1,14 @@
+
+class Network(object):
+
+    def __init__(self):
+        pass
+
+    def send(self, msg, agent_id):
+        pass
+
+    def braodcast(self, msg):
+        pass
+
+    def next_msg(self):
+        pass
